affc89fef3f84b829ec69e7643df9e0fdc194a2e
akmu146 <161188013+akmu146@users.noreply.github.com>
2025-02-08 12:27:35 +1300

    Completed skeleton code - still need to do several test cases

---
+++ Testing/src/ATM.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+    public static void main(String[] args) {
+        OptionMenu optionMenu = new OptionMenu();
+
+        try {
+            optionMenu.getLogin();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }    
+}
